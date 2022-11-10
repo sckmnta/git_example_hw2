7863e42eb811b19947d3d74d9031f4298270fec2
Nikita Maniakin <scksckscksckscksck@gmail.com>
2022-11-11 01:17:05 +0300

    Nice things removed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Nice to see you!");
+        System.out.println("Happy to see you!");
     }
 }
